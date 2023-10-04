61e2bd9c7342f313cc40e959ebadf52ceb91ee10
Lucas Holder <78889661+lucas-j-holder@users.noreply.github.com>
2023-10-03 17:45:05 -0700

    Removed invalid import.

--- logic/EditFinancialDialog.py
+++ logic/EditFinancialDialog.py
@@ -2,7 +2,6 @@ from PyQt6.QtWidgets import QDialog, QWidget
 from ui.Ui_EditFinancial import Ui_EditFinancial
 from logic.models.FinancialModel import FinancialModel
 from logic.enums.Frequencies import Frequencies
-from multipledispatch import dispatch
 from multimethod import multimethod, overload
 
 
